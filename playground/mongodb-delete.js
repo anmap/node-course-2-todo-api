8b297501284b1c46ceaffce89eb39c6b2c120aa9
anmap <timeof2010@gmail.com>
2017-08-28 14:44:26 +0300

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+// ES6 obj destructuring
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+
+// Connect to db
+// connect(url, callback)
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany(
+    //     { text: 'Eat lunch' }
+    // ).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne(
+    //     { text: 'Eat lunch' }
+    // ).then((result) => {
+    //     console.log(result);
+    // })
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({
+        completed: false
+    }).then((result) => {
+        console.log(result);
+    })
+
+    // Close the connection
+    db.close();
+});
