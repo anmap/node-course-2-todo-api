15c473b0c635456cb6d0c857a7559e76cd502d24
anmap <timeof2010@gmail.com>
2017-08-30 01:37:29 +0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Mongoose.connect(host, { useMongoClient: true});
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
 });
 
