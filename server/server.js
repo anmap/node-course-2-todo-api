d07d4b9626f816647e73e602f45fa14087b3989d
anmap <timeof2010@gmail.com>
2017-08-31 02:10:23 +0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -100,6 +100,19 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
+app.post('/users', function(req, res) {    
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch(e => res.status(400).send(e));
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
